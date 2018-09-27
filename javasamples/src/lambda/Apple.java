6475cfdbe5dba33f665935f771980e38ce59607b
arunveersingh <arunveersingh@arunveersingh.me>
2018-09-27 15:12:01 +0530

    Update Apple POJO to support examples I am using for exploring generics.

--- javasamples/src/lambda/Apple.java
+++ javasamples/src/lambda/Apple.java
@@ -1,10 +1,37 @@
 package lambda;
 
 public class Apple {
-
-	public String getWeight() {
-		// TODO Auto-generated method stub
-		return null;
+	
+	private int weight;
+	private String color;
+	
+	boolean tested;
+	
+	public Apple(int weight, String color) {
+		this.weight = weight;
+		this.color = color;
 	}
+	
+	public int getWeight() {
+		return weight;
+	}
+	public String getColor() {
+		return color;
+	}
+
+	public boolean isTested() {
+		return tested;
+	}
+
+	public void setTested(boolean tested) {
+		this.tested = tested;
+	}
+
+	@Override
+	public String toString() {
+		return "Apple [weight=" + weight + ", color=" + color + ", tested=" + tested + "]";
+	}
+	
+	
 
 }
